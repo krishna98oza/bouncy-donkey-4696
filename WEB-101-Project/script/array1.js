06fcaa16bbf2ee5fa7cfcbc048f930f43aa07568
unknown <krish98oza.com>
2023-03-30 23:33:37 +0530

    login and singin pages added

--- WEB-101-Project/script/array1.js
+++ WEB-101-Project/script/array1.js
@@ -1,2 +1,145 @@
 // array for home page new arrival part
 
+let products = 
+[
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "Khaki Beige Pilot Backpack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/dailyobjects-khaki-beige-pilot-backpack-images/DailyObjects-Khaki-Beige-City-Compact-Backpack.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 2199,
+        prodId : 2
+    },
+    {
+        title : "Amber Taiga Backpack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/amber-taiga-backpack-images/Amber-Taiga-Backpack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 3999,
+        prodId : 3
+    },
+    {
+        title : "Green Scoot Backpack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/green-scoot-backpack-images/Green-Scoot-Backpack.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1299,
+        prodId : 4
+    },
+    {
+        title : "Ivory - Navy 9 to 9 Backpack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/natural-white---navy-9-to-9-backpack-images/Natural-White-Navy-9-to-9-Backpack.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 2499,
+        prodId : 5
+    },
+    {
+        title : "Hunter Green Card Wallet",
+        img : "https://images.dailyobjects.com/marche/product-images/1302/dailyobjects-hunter-green-skinnyfit-card-wallet-images/DailyObjects-Hunter-Green-SkinnyFit-Card-Wallet-vw.png?tr=cm-pad_resize,v-2,w-187,h-230,dpr-1",
+        category : "wallet",
+        price : 499,
+        prodId : 6
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+    {
+        title : "All Red Pedal Daypack",
+        img : "https://images.dailyobjects.com/marche/product-images/1201/all-red-pedal-daypack-images/All-Red-Pedal-Daypack-vw.png?tr=cm-pad_resize,v-2,w-444,h-598,dpr-1",
+        category : "backpack",
+        price : 1699,
+        prodId : 1
+    },
+]
